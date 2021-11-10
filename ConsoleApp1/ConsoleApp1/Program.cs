daae5ddd54dea29ce6c0c998919e2eb458f7aa4a
PKKM1998 <pmudemypls@gmail.com>
2021-11-10 04:12:44 +0100

    First Commit with Second Branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Parcels
+{
+    static void Main(string[] args)
+    {
+        int a, b, c;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        c = int.Parse(Console.ReadLine());
+        if (a*a+b*b==c*c) { Console.WriteLine("OK"); }
+        else { Console.WriteLine("Not OK"); }
+    }
+}
