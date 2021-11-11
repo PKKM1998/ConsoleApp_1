f7d020e7c38f8609b0931c8e2d837e6bc293c123
PKKM1998 <pmudemypls@gmail.com>
2021-11-11 06:02:30 +0100

    more exercises

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 namespace NewApp;
+class Producer
+{
+    public string ppname;
+    public string swname;
+    public int cyint;
+    public double ssstatus;
+    public string companystatus = "active";
+    public Producer(string ProducerName, string SoftwareName, int CreationYear, double StockStatus)
+    {
+        ppname = ProducerName;
+        swname = SoftwareName;
+        cyint = CreationYear;
+        ssstatus = StockStatus;
+    }
+    public void getDataProducer()
+    {
+        Console.WriteLine(ppname + "\n" + swname + "\n" + cyint + "\n" + ssstatus + "\n" + companystatus);
+    }
+    class ProducerStatus : Producer
+    {
+        public ProducerStatus(string ProducerName, string SoftwareName, int CreationYear, double StockStatus, string corpstatus) : base(ProducerName, SoftwareName, CreationYear, StockStatus)
+        {
+            companystatus = corpstatus;
+        }
+    }
+}
 class Program
 {
     public string name;
@@ -16,42 +42,12 @@ class Program
     {
         Console.WriteLine(name + "\n" + producer + "\n" + relase);
     }
-    //static int NewMethod(int a, int b)
-    //{
-    //    return a + b;
-    //}
-    //static double NewMethod(double a, double b)
-    //{
-    //    return a + b;
-    //}
     static void Main(string[] args)
     {
-        //int a, b, c;
-        //a = int.Parse(Console.ReadLine());
-        //b = int.Parse(Console.ReadLine());
-        //c = int.Parse(Console.ReadLine());
-        //if (a * a + b * b == c * c) { Console.WriteLine("OK"); }
-        //else { Console.WriteLine("Not OK"); }
-        //int[] tab1 = {1, 3, 4, 5, 10};
-        //Array.Sort(tab1);
-        //   foreach (int i in tab1)
-        //{
-        //   Console.WriteLine(i);
-        //}
-        //Program Program1 = new();
-        //Program1.a = "ab";
-        //Program1.b = "ba";
-        //Program1.c = 2010;
-        //Program Program2 = new();
-        //Program2.a = "aba";
-        //Program2.b = "bab";
-        //Program2.c = 2015;
-        //Console.WriteLine(tab1.Max());
-        //Console.WriteLine(NewMethod(5, 7));
-        //Console.WriteLine(NewMethod(6.5, 6.4));
+        Producer Producer1 = new("XD", "XD", 1405, 302);
         Program Program1 = new("Sonic", "SonicEntertaiment", 1998);
         Program Program2 = new("Vortex", "VortexSolution\'s", 2004);
-
+        Producer1.getDataProducer();
         Program1.getData();
         Program2.getData();
     }
